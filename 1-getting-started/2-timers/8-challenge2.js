3efaa2e02a3df8788f9d20971cc9d7c203cb96b1
Raimondas-Alisauskas <ra.alisa.123@gmail.com>
2019-07-20 12:20:56 +0300

    timer2

--- 1-getting-started/2-timers/8-challenge2.js
+++ 1-getting-started/2-timers/8-challenge2.js
@@ -3,3 +3,16 @@
 // And stop after 5 times
 
 // After 5 times. Print "Done" and let Node exit.
+
+let i = 1;
+const task = () => {
+    console.log("Hello World");
+    i += 1;
+
+    if (i > 5){
+        clearTimeout(taskAtInterval);
+        console.log("Done");
+    }
+}
+
+let taskAtInterval = setInterval (task, 1000)
